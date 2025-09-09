da179909df87bd502d593c6f9d227b983fc2063b
asainos2006lgtm <asainos2006@gmail.com>
2025-09-09 14:58:06 -0600

    Tipos de datos
    Se explico cuales son los tipos de datos y se usaron ejemplos

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,14 @@
-document.write("<h1>Hello World</h1>")
+//Tipos de datos 
+
+var nameuser = "Juan"; // String
+let lastname = 'Perez'; // String
+
+nameuser = "Carlos"; // Se reasigna la variable
+
+const PI = 3.1416; // Number/ Variable constante por lo cual no se puede cambiar su valor, si se quiere modificar aparecera un error 
+
+
+
+console.log(nameuser);
+
+console.log(PI);
