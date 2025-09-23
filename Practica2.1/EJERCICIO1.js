97af617f67e9c6233ea868c97200e607b3866ad8
asainos2006lgtm <asainos2006@gmail.com>
2025-09-22 22:10:21 -0600

    Destructuración

---
+++ Practica2.1/EJERCICIO1.js
@@ -0,0 +1,16 @@
+// Objeto persona
+const persona = {
+  nombre: "Iván Isay",
+  edad: 37,
+  direccion: {
+    ciudad: "Qro",
+    pais: "México"
+  }
+  
+};
+
+// Destructuración
+const { nombre, edad, direccion: {ciudad, pais}} = persona;
+
+// Mensaje
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ", " + pais + ".");
