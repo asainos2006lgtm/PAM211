66064f1c139dcbd9bea42cec2a980bc041a05757
asainos2006lgtm <asainos2006@gmail.com>
2025-10-08 10:26:25 -0600

    Creación de un contador y configuración de botones para dicho contador
    Se creo un contador con tres botones, uno para agregar al contador, otro para quitar y uno para reiniciarlo

--- INTRO/App.js
+++ INTRO/App.js
@@ -1,17 +1,27 @@
+// 1. Imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
+
+//2. Main: Zona de componentes 
 export default function App() {
+
+      const [contador, setContador] = useState(0);
+
   return (
+
     <View style={styles.container}>
-      <Text>HOLA MUNDO REACT NATIVE!!
-        
-      </Text>
+      <Text>Contador: {contador}</Text>
+      <Button title='Agregar' onPress={()=>setContador(contador + 1)}/>
+      <Button title='Quitar' onPress={()=>setContador(contador - 1)}/>
+      <Button title='Reiniciar' onPress={()=>setContador(0)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+ 
+// Estilos: Zona estetica prara componentes
 const styles = StyleSheet.create({
   container: {
     flex: 1,
