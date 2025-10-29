1155ab2012c9acd2a18c4e9f2b6ddf076557a421
asainos2006lgtm <asainos2006@gmail.com>
2025-10-28 22:44:10 -0600

    Practica No.13: ActivityIndicator & List’s
    Se realizo y completo la practica No.13: ActivityIndicator & List’s

--- INTRO/Screen2/Indicador.js
+++ INTRO/Screen2/Indicador.js
@@ -1,28 +1,48 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React,{useState} from 'react';
+import { Text, StyleSheet, View, Alert, Platform, ActivityIndicator, Button} from 'react-native'
+import React, {useState} from 'react';
 
 export default function Indicador () {
+  const [cargando, setCargando] = useState(false);
+  const carga = () => {
+    setCargando(true);
+    setTimeout(() =>{
+      setCargando(false);
+      if(Platform.OS === 'web'){
+      window.alert('Carga completa');
+      }else{
+        Alert.alert('Carga completa');
+      }
+    }, 3000);
+  }
   
     return (
       <View style={styles.container}>
-        <Text style = {styles.texto}>Proximamente......</Text>
+        <Text style = {styles.texto}>Presione para iniciar la carga</Text>
+        <View style={styles.botonesContainer}>
+          {cargando ? (<ActivityIndicator size='large' color='red'></ActivityIndicator>) : (<Button color='brown' title='Presione para iniciar' onPress={carga}></Button>)}
+        </View>
       </View>
     )
   
 }
 
 const styles = StyleSheet.create({
-          container : {
+        container : {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#8e7d7d7a',
+        backgroundColor: '#ffff',
     },
         texto :{
-        fontSize:60,
+        fontSize:30,
         color:'#000000df',
         fontFamily:'Times New Roman',
         fontWeight:'bold',
         foontStyle:'italic',
+  },
+  botonesContainer : {
+    marginTop:20,
+    gap : 20,
+
   }
 })
--- INTRO/Screen2/Listas.js
+++ INTRO/Screen2/Listas.js
@@ -1,12 +1,59 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, FlatList, SectionList } from 'react-native'
 import React,{useState} from 'react';
 
 export default function Listas () {
+  //FlatList
+  const ejercicios= [
+    {id: '1', nombre: 'Sentadillas', descripcion: 'Ejercicio para las piernas y glúteos'},
+    {id: '2', nombre: 'Flexiones', descripcion: 'Ejercicio para el pecho y brazos'},
+    {id: '3', nombre: 'Abdominales', descripcion: 'Ejercicio para el abdomen'},
+    {id: '4', nombre: 'Burpees', descripcion: 'Ejercicio de cuerpo completo'},
+    {id: '5', nombre: 'Zancadas', descripcion: 'Ejercicio para las piernas y glúteos'},
+    {id: '6', nombre: 'Plancha', descripcion: 'Ejercicio para el core'},
+    {id: '7', nombre: 'Saltos de tijera', descripcion: 'Ejercicio cardiovascular'},
+    {id: '8', nombre: 'Mountain climbers', descripcion: 'Ejercicio para el core y cardio'},
+    {id: '9', nombre: 'Superman', descripcion: 'Ejercicio para la espalda baja'},
+    {id: '10', nombre: 'Puente de glúteos', descripcion: 'Ejercicio para glúteos y espalda baja'},
+  ]
+  //SectionList
+  const contactos = [
+    {titulo: 'A', data: ['Alejandra', 'Ana la de las torillas', 'Adele']},
+    {titulo: 'B', data: ['Brenda', 'Beto', 'Bárbara']},
+    {titulo: 'C', data: ['Carlos', 'Cecilia', 'Camilo']},
+    {titulo: 'D', data: ['Daniel', 'Diana', 'Dario']},
+  ]
   
     return (
       <View style={styles.container}>
-        <Text style = {styles.texto}>Proximamente......</Text>
+       
+        <View style={styles.listContainer}>
+          <Text style={styles.title}>Lista de Ejercicios</Text>
+          <FlatList 
+          data={ejercicios}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <Text style={styles.nombre}>{item.nombre}</Text>
+              <Text style={styles.descripcion}>{item.descripcion}</Text>
+            </View>
+          )}
+          />
+        </View>
+     
+        <View style={styles.listContainer}>
+          <Text style={styles.title}>Contactos</Text>
+          <SectionList
+          sections={contactos} 
+          renderItem={({item}) => (
+            <Text style={styles.item}>{item}</Text>
+          )}     
+          renderSectionHeader={({section}) => {
+            <Text style={styles.header}>{section.titulo}</Text>
+          }}    
+          />
+        </View>
       </View>
+
+      
     )
   
 }
@@ -14,15 +61,38 @@ export default function Listas () {
 const styles = StyleSheet.create({
           container : {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#8e7d7d7a',
+        padding: 10,
     },
-        texto :{
-        fontSize:60,
-        color:'#000000df',
-        fontFamily:'Times New Roman',
-        fontWeight:'bold',
-        foontStyle:'italic',
-  }
+        listContainer : {
+      flex: 1,
+      marginBottom: 20
+    },
+    title :{
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginBottom: 10,
+    },
+    item : {
+      padding: 10,
+      backgroundColor : '#c3c3c3ff',
+      marginVertical: 5,
+      marginHorizontal: 10,
+      borderRadius: 5,
+    },
+    nombre : {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 5,
+    },
+    descripcion:{
+      fontSize: 14,
+      color: '#666',
+    },
+    header : {
+      fontSize: 18,
+      backgroundColor: '#white',
+      padding: 10,
+      marginTop: 10,
+    },
 })
