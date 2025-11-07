f15f40018f1ca104570d0306035a4af7ddb9fba7
asainos2006lgtm <asainos2006@gmail.com>
2025-11-06 19:51:21 -0600

    1er commit 19:50

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,27 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, StyleSheet, View, Image, ImageBackground, Dimensions } from 'react-native'
+import React, { useState, useEffect } from 'react'
 
-export default function App() {
+
+const BackgroundImage = require('./assets/images.png');
+
+export default function App({navegation}) {
+      const [showSplash, setShowSplash] = useState(true);
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setShowSplash(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }, []);
+    if(showSplash){
   return (
-    <View style={styles.container}>
-      <Text style = {styles.texto}>Hola mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+    source={BackgroundImage}
+    styles={styles.background}
+    resizeMode='cover'    
+    >
+      <View style= {styles.overlay}>
+          <Text style={styles.title}>Bienvenido</Text>
+          <Text style={styles.title}>Cargando.....</Text>
+        </View>
+    </ImageBackground>
+
+
+    )
+  }
+
+  return (
+    <ImageBackground
+    source={BackgroundImage}
+    style={styles.background}
+    resizeMode='cover'
+    >
+      <View
+       style={styles.mainScreen}
+      >
+        <Text style={styles.maintexto}>Pantalla Principal</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
+const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  background: {
+    width: width,
+    height: height,
   },
-  texto : {
-    fontsize: 50,
-    color: '#000000df',
-    fontFamily: 'Times New Roman',
+  overlay:{
+    flex : 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  mainScreen:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    bakcgroudnColor:'#f2f2f2',
+  },
+  maintexto:{
+    fontSize:30,
+    color:'#333',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
     fontWeight: 'bold',
-    foontStyle: 'italic',
+    marginBotton: 10,
+    textAlign: 'center'
+
   }
 });
